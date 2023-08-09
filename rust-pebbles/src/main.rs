8167e75bf662733a8d8a50c762327e2d113e8a1b
tyler <tktaormina@gmail.com>
2023-08-09 14:18:20 -0700

    update

--- rust-pebbles/src/main.rs
+++ rust-pebbles/src/main.rs
@@ -29,15 +29,43 @@ fn fp2(r: i32, c: i32, mut t: [[i32; 5]; 4]) -> i32 {
     }
 }
 
+fn fp3(r: i32, c: i32, mut t: [[i32; 5]; 4]) -> i32 {
+    match (r, c) {
+        (0, 0) => BOARD[r as usize][c as usize],
+        (a, b) => {
+            for i in 0..=a {
+                for j in 0..=b {
+                    match (i, j) {
+                        (0, 0) => t[0][0] = BOARD[0][0],
+                        (0, x) => t[0][x as usize] = t[0][(x - 1) as usize] + BOARD[0][x as usize],
+                        (x, 0) => t[x as usize][0] = t[(x - 1) as usize][0] + BOARD[x as usize][0],
+                        (x, y) => {
+                            t[x as usize][y as usize] = (std::cmp::max(
+                                t[(x - 1) as usize][y as usize],
+                                t[x as usize][(y - 1) as usize],
+                            )) + BOARD[x as usize][y as usize];
+                        }
+                    }
+                }
+            }
+            t[r as usize][c as usize]
+        }
+    }
+}
+
 fn main() {
-    let _mtable = [[-1; 5]; 4];
+    let mtable = [[-1; 5]; 4];
     let ttable = [[-1; 5]; 4];
 
     let a: i32 = fp1(3, 4);
     assert_eq!(a, 5);
     println!("Naive recursive solution: {}", a);
 
-    let b: i32 = fp2(3, 4, ttable);
-    assert_eq!(a, 5);
+    let b: i32 = fp2(3, 4, mtable);
+    assert_eq!(b, 5);
     println!("Memoized recursive solution: {}", b);
+
+    let c: i32 = fp3(3, 4, ttable);
+    assert_eq!(c, 5);
+    println!("Tabulized iterative solution: {}", c);
 }
