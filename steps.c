34943927e0cc47cb952a43f55e2e596d871f7eba
tyler <tktaormina@gmail.com>
2023-08-11 11:32:03 -0700

    finished n choose k problem with 2 good solutions and 1 naive recursive solution. need to write rod cutting next

--- steps.c
+++ steps.c
@@ -15,6 +15,17 @@ int t[N][N + 1] = {
   {8, 6, 9, 6, INF}
 };
 
+int ct[7 + 1][7 + 1] = {
+  {0, 0, 0, 0, 0, 0, 0, 0}, 
+  {0, 0, 0, 0, 0, 0, 0, 0}, 
+  {0, 0, 0, 0, 0, 0, 0, 0}, 
+  {0, 0, 0, 0, 0, 0, 0, 0}, 
+  {0, 0, 0, 0, 0, 0, 0, 0}, 
+  {0, 0, 0, 0, 0, 0, 0, 0}, 
+  {0, 0, 0, 0, 0, 0, 0, 0}, 
+  {0, 0, 0, 0, 0, 0, 0, 0}
+};
+
 
 // Fibonacci sequence
 int
@@ -23,6 +34,50 @@ steps(int num)
   if (num == 1) return 1;
   if (num == 2) return 2;
   return (steps(num - 1) + steps(num - 2));
+}
+
+int
+choose(int n, int k)
+{
+  if (n == k) return 1;
+  if (k == 0) return 1;
+  return choose(n - 1, k - 1) + choose(n - 1, k);
+}
+
+int
+choose_memo(int n, int k)
+{
+  if (ct[n][k] != 0) return ct[n][k];
+  if (n == k || k == 0){
+    ct[n][k] = 1;
+    ct[n][n-k] = 1;
+    return ct[n][k];
+  } 
+  ct[n][k] = choose_memo(n - 1, k - 1) + choose_memo(n - 1, k);
+  ct[n][n-k] = ct[n][k];
+  return ct[n][k];
+}
+
+int
+choose_tab(int n, int k)
+{
+  int i, j;
+  if (n == k || k == 0) return 1;
+  if (k > n) return 0;
+  for (i = 0; i <= n; i++) {
+
+    // Instead of calculating the full pascals triangle we can calculate just what we need
+    // by substituting in the line below for the j portion of the for loop. 
+  
+    // for (j = 0; j <= i && j <= k; j++) {
+    for (j = 0; j <= i ; j++) {
+      if (j == 0 || j == i) ct[i][j] = 1;
+      else {
+        ct[i][j] = ct[i-1][j] + ct[i-1][j-1];
+      }
+    }
+  }
+  return ct[n][k];
 }
 
 int
@@ -80,27 +135,33 @@ int
 main(int argc, char *argv[])
 {
   int i, j;
-  //int ans = steps(5);
+  int ans = steps(5);
   int ans1 = sum_descent(0, 0);
   int ans2 = sum_descent_memo(0, 0);
-  int ans3 = sum_descent_tab(3, 3);
+  int ans3 = sum_descent_tab(0, 0);
+  int ans4 = choose(7, 3);
+  // int ans5 = choose_memo(7, 6);
+  int ans6 = choose_tab(7, 3);
 
-  //printf("Answer steps   : %d\n", ans);
-  printf("Answer sum     : %d\n", ans1);
-  printf("Answer memo sum: %d\n", ans2);
-  printf("Answer tab sum : %d\n", ans3);
+  printf("Answer steps      : %d\n", ans);
+  printf("Answer sum        : %d\n", ans1);
+  printf("Answer memo sum   : %d\n", ans2);
+  printf("Answer tab sum    : %d\n", ans3);
+  printf("Answer n choose k : %d\n", ans4);
+  // printf("Answer memo choose: %d\n", ans5);
+  printf("Answer tab choose : %d\n", ans6);
 
 
   // Shows restructured array
-  /*
-  for (i = 0; i < N; i++) {
-    for (j = 0; j < N + 1; j++) {
-      if (t[i][j] != INF) printf("%d ", t[i][j]);
+  // /*
+  for (i = 0; i < 8; i++) {
+    for (j = 0; j < 8; j++) {
+      printf("%d ", ct[i][j]);
     }
     printf("\n");
   }
   printf("\n");
-  */
+  // */
 
   return EXIT_SUCCESS;
 }
